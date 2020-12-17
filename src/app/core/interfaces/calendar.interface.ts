b1704cdbe0912d63899f42e35211daec67cf08f9
Sasha <wwesasha12@gmail.com>
2020-12-17 12:14:32 +0200

    v.1.1

---
+++ src/app/core/interfaces/calendar.interface.ts
@@ -0,0 +1,12 @@
+import * as moment from 'moment';
+
+export interface Week {
+  start: moment.Moment,
+  end: moment.Moment,
+  days: Day[]
+}
+
+export interface Day {
+  value: moment.Moment,
+  selected: boolean
+}
---
+++ src/app/core/service/date.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import * as moment from 'moment';
+import {BehaviorSubject} from 'rxjs';
+import {Week} from '../interfaces/calendar.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DateService {
+  public date: BehaviorSubject<moment.Moment> = new BehaviorSubject(moment())
+
+  public changeWeek(direction: number):void {
+    const val = this.date.value.add(direction, 'w')
+    this.date.next(val)
+  }
+
+  public getWeek(now: moment.Moment): Week {
+    const startDay = now.clone().startOf('week')
+    const endDay = now.clone().endOf('week')
+
+    const date = startDay.clone().subtract(1, 'day')
+
+    const week: Week = {
+      start: startDay,
+      end: endDay,
+      days: []
+    }
+
+    while (date.isBefore(endDay, 'day')) {
+      week.days.push({
+        value: date.add(1, 'day').clone(),
+        selected: false
+      })
+    }
+    return week
+  }
+
+
+}
+
+
